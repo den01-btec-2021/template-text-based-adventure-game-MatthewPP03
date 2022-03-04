ab034f40136e96f4979aaaa2efe44f144ec2b375
Jack Phillips <9275@bridgend.ac.uk>
2022-03-04 13:26:16 +0000

    Added Directions

--- src/main.py
+++ src/main.py
@@ -3,7 +3,18 @@ def main():
     player_name=input("What is your name?")
     print("Hi" + player_name)
 
-lives = 3
-print(f"You have {lives}lives remaining")
+    lives = 3
+    print(f"You have {lives} lives remaining")
+
+    direction = input("Which direction would you like to go?")
+
+    if direction == "North": 
+        print("You have now travelled North!")
+    elif direction == "South":
+        print("You have travelled South!")
+    elif direction == "West":
+        print("You have now travelled West!")
+    else:
+        print("You have travelled East!")
 
 main()
